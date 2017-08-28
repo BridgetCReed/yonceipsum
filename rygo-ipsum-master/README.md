a96c78a8b0487a9820c038374c6863657a080d5d
Bridget Reed <bridgetreed@Bridgets-MacBook-Pro-2.local>
2017-08-28 18:01:29 -0400

    ubutuak

---
+++ rygo-ipsum-master/README.md
@@ -0,0 +1,14 @@
+rygo-ipsum
+==========
+
+Ryan Gosling lorem ipsum, featured on [Meet The Ipsums](http://meettheipsums.com/)!
+
+http://www.rygo-ipsum.com/
+
+### Development ###
+
+Clone or fork this repo, fire up a server from the project directory, 
+
+`$ python -m SimpleHTTPServer 3000`
+
+and head to http://localhost:3000 (or whatever port you choose for your server) to get started!
---
+++ rygo-ipsum-master/js/site.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+  /**
+  * Remove any existing selected states, and change a button's text color to 
+  pink if it was clicked 
+  * @param {string} selector - DOM element that triggers event
+  */
+  function addButtonSelectedState(selector) {
+    $('.buttons li').removeClass('salmon');
+    $(selector).addClass('salmon');
+  }
+
+  /**
+  * Create a multi-dimensional array of random ipsum text. Length is determined
+  * by the value of the paragraph & sentence params 
+  * @param {number} paragraphCount - number of paragraphs generated
+  * @param {number} sentenceCount - number of sentences generated in each paragraph
+  */
+  function generateIpsum(paragraphCount, sentenceCount){
+    $.getJSON('sentences.json',
+      function(data) {
+        var sentenceArr = _.shuffle(data);
+        var firstSentences = [];
+        var paragraphArr = [];
+
+        for (var p=0; p < paragraphCount; p++) {
+          firstSentences = _.first(sentenceArr, sentenceCount);
+          paragraphArr.push(firstSentences);
+          sentenceArr = _.rest(sentenceArr, sentenceCount);
+        }
+
+        return generateTemplate(paragraphArr);
+    });
+  }
+
+  /**
+  * Create and insert markup based on data from the ipsum array
+  * @param {Array} ipsum - multi-dimensional array of paragraphs, sentences
+  */
+  function generateTemplate(ipsum) {
+    var templateString = $('.template').html();
+    var template = _.template(templateString)({ipsum: ipsum});
+    $('.text').html(template);
+  }
+
+  /**
+  * Attach an event listener that calls functions to change a button's 
+  * selected state & generate the text block
+  * @param {string} selector - DOM element that triggers event
+  * @param {number} paragraphCount - number of paragraphs generated
+  * @param {number} sentenceCount - number of sentences generated in each paragraph
+  */
+  function toggleIpsum(selector, paragraphCount, sentenceCount) {
+    $(selector).on('click', function() {
+      addButtonSelectedState(selector)
+      generateIpsum(paragraphCount, sentenceCount);
+    })
+  }
+
+  toggleIpsum('.short', 1, 8);
+  toggleIpsum('.medium', 3, 5);
+  toggleIpsum('.long', 5, 5);
+  
+});
